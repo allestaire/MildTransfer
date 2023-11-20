a0402c5f06484a8a17f53d8407c14e3d9c05688a
Al Lestaire Acasio <allestaire.acasio@ustp.edu.ph>
2023-11-20 22:52:14 +0800

    :pencil2: Middleware messages

--- src/Lang/en.js
+++ src/Lang/en.js
@@ -1,5 +1,12 @@
 module.exports = {
   enums: {
     ACCESS_TOKEN: 'mildtransfer'
+  },
+  api: {
+    middleware: {
+      UNAUTHORIZED_ACCESS: 'Unauthorized access.',
+      INVALID_CREDENTIAL: 'Invalid credential',
+      SERVER_ERROR: 'Got some server error.'
+    }
   }
 }
