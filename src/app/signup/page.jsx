3612fdef05b60a3439f2f7c53f955a0f90a45c1f
Al Lestaire Acasio <allestaire.acasio@ustp.edu.ph>
2023-11-20 21:43:08 +0800

    :lipstick: Validation

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,8 +1,42 @@
 'use client'
 
+import { useFormik } from "formik"
+import * as Yup from 'yup'
+
 
 const Signup = () => {
+  const schema = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      password: '',
+      confirmpassword: '',
+    },
+    validationSchema: Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().required().email(),
+      password: Yup.string().required()
+        .test(
+          'is-strong',
+          ({ label }) => 'The password field is not strong.',
+          (value) => {
+            return /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)/.test(value)
+          }
+        ),
+      confirmpassword: Yup.string().required()
+        .test(
+          'is-matched',
+          ({ label }) => 'The password field does not match',
+          (value) => {
+            return value === schema.values.password
+          }
+        )
 
+    }),
+    onSubmit(values) {
+      console.log(values)
+    }
+  })
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -22,7 +56,31 @@ const Signup = () => {
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Become one of us!
             </h1>
-            <form className="space-y-4 md:space-y-6" action="#">
+            <form className="space-y-4 md:space-y-6" action="#" method="POST" onSubmit={schema.handleSubmit}>
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Tell us your name
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="John Doe"
+                  value={schema.values.name}
+                  onChange={schema.handleChange}
+                />
+                {
+                  schema.touched.name && schema.errors.name && (
+                    <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                      {schema.errors.name}
+                    </p>
+                  )
+                }
+              </div>
               <div>
                 <label
                   htmlFor="email"
@@ -36,8 +94,16 @@ const Signup = () => {
                   id="email"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="name@company.com"
-                  required=""
+                  value={schema.values.email}
+                  onChange={schema.handleChange}
                 />
+                {
+                  schema.touched.email && schema.errors.email && (
+                    <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                      {schema.errors.email}
+                    </p>
+                  )
+                }
               </div>
               <div>
                 <label
@@ -52,8 +118,16 @@ const Signup = () => {
                   id="password"
                   placeholder="••••••••"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
+                  value={schema.values.password}
+                  onChange={schema.handleChange}
                 />
+                {
+                  schema.touched.password && schema.errors.password && (
+                    <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                      {schema.errors.password}
+                    </p>
+                  )
+                }
               </div>
               <div>
                 <label
@@ -63,13 +137,21 @@ const Signup = () => {
                   Confirm Password
                 </label>
                 <input
-                  type="confirmpassword"
-                  name="password"
+                  type="password"
+                  name="confirmpassword"
                   id="confirmpassword"
                   placeholder="••••••••"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
+                  value={schema.values.confirmpassword}
+                  onChange={schema.handleChange}
                 />
+                {
+                  schema.touched.confirmpassword && schema.errors.confirmpassword && (
+                    <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                      {schema.errors.confirmpassword}
+                    </p>
+                  )
+                }
               </div>
               <button
                 type="submit"
