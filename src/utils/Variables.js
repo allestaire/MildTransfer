721c901ec7987d62d83eab10ff89155c19946902
Al Lestaire Acasio <allestaire.acasio@ustp.edu.ph>
2023-11-21 08:55:16 +0800

    :wrench: Added mail env

--- src/utils/Variables.js
+++ src/utils/Variables.js
@@ -2,5 +2,8 @@ export default {
   API_URL: process.env.API_URL || '',
   IS_DEV: process.env.IS_DEV,
   DEFAULT_PASSWORD: process.env.DEFAULT_PASSWORD || '',
-  PASSWORD_SALT: process.env.PASSWORD_SALT || 10
+  PASSWORD_SALT: process.env.PASSWORD_SALT || 10,
+  BASE_URL: process.env.BASE_URL || '',
+  MAIL_HOST: process.env.MAIL_HOST || '',
+  MAIL_PORT: process.env.MAIL_PORT || ''
 }
