aed10f5322b94e590403f9e8c320ebb7f6d06003
Al Lestaire Acasio <allestaire.acasio@ustp.edu.ph>
2023-11-23 22:37:40 +0800

    :sparkles: Devices api

---
+++ src/app/api/auth/devices/route.js
@@ -0,0 +1,26 @@
+import Helper from "@/utils/Helper"
+import Lib from "@/utils/Lib"
+import Prisma from "@/utils/Prisma"
+import { NextResponse } from "next/server"
+
+
+export async function GET(request) {
+  try {
+    const token = (request.headers.get('authorization') || '').replace(/bearer\s?/i, '')
+    const user = await Lib.getCurrentUser(token)
+    const users = await Prisma.Client.user.findMany({
+      where: {
+        id: {
+          not: user.id
+        },
+        verified_at: {
+          not: null
+        },
+        deactivated_at: null
+      }
+    })
+    return NextResponse.json(users)
+  } catch (e) {
+    return Helper.handleException(e)
+  }
+}
